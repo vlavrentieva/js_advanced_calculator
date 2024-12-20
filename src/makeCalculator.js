9b82d311e868dcbb7780eea39d281c411556653a
Vlada Lavrentieva <vlada.lavr24@gmail.com>
2024-12-20 14:38:40 +0100

    Calculator created

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(number) {
+      calculator.result += number;
+    },
+    subtract(number) {
+      calculator.result -= number;
+    },
+    multiply(number) {
+      calculator.result *= number;
+    },
+    divide(number) {
+      calculator.result /= number;
+    },
+    reset(number) {
+      calculator.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback(number);
+
+      return this;
+    },
+  };
+
+  // calculator.operate = (callback, number) => {
+  // return callback(number);
+  // }
+
+  // calculator.add = (number) => (calculator.result += number);
+  // calculator.subtract = (number) => (calculator.result -= number);
+  // calculator.multiply = (number) => (calculator.result *= number);
+  // calculator.divide = (number) => (calculator.result /= number);
+  // calculator.reset = (number) => calculator.result === 0;
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
